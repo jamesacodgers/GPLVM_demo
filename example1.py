acfa366e5eeb63f6ef44f148a1e341b204037669
James <jo816@ic.ac.uk>
2021-10-25 13:03:23 +0100

    git commit

--- example1.py
+++ example1.py
@@ -9,11 +9,11 @@ D = 2                           # Number of observed dimensions
 q = 1                           # Number of latent dimensions
 N = 100                         # Number of data points
 
-beta = 1                       # Scale parameter for variance of noise 
+beta = 0.1                       # Scale parameter for variance of noise 
 
 X = rand.normal(0,1,N)          # Generate true latent variables
 
-W = np.array([np.sqrt(2),np.sqrt(2)])             # Linear mapping from x to y 
+W = np.array([np.sqrt(8),np.sqrt(8)])             # Linear mapping from x to y 
 
 Y = np.outer(X,W) + rand.normal(0,1/beta, (N,2))               # Map to other latent variable 
 
@@ -25,18 +25,21 @@ plt.scatter(Y[:,0],Y[:,1])
 plt.show()
 # %%
 
-eigval, eigvec = lin.eig(Y @ Y.T/D)
+eigval, eigvec = lin.eig(Y @ Y.T)
 
 i = np.argmax(np.real(eigval))
 
 
 
-X_est = np.real(eigvec[:,i])*(np.real(eigval[i]))**(1/2)
-print(X_est, (np.real(eigval[i]))**(1/2))
+X_est = np.real(eigvec[:,i])*(np.real(eigval[i])/D)**(1/2)
+X_prior = np.real(eigvec[:,i])*(np.real(eigval[i])/D - 1/beta)**(1/2)
+#print(X_est, (np.real(eigval[i]) - 1/beta)**(1/2))
 
 plt.figure()
 plt.scatter(X,X_est)
+plt.scatter(X,X_prior)
 plt.xlabel("true")
+plt.ylabel("found")
 plt.plot(np.linspace(-3,3),np.linspace(-3,3), c = "orange")
 plt.show()
 
